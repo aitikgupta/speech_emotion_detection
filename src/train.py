eeb8131822261d072d414bc96586f9b3b31713c6
Aitik Gupta <aitikgupta@gmail.com>
2020-04-12 10:09:10 +0530

    Updated underlying model

--- src/train.py
+++ src/train.py
@@ -21,52 +21,40 @@ import numpy as np
 
 def give_model():
     model = Sequential()
-    model.add(Conv1D(1024, 2, input_shape=(315, 221), padding="same"))
+
+    model.add(LSTM(256, recurrent_dropout=0.2, input_shape=(315, 221), kernel_initializer= glorot_uniform(), return_sequences=True))
+    model.add(Conv1D(512, 10, padding="same", activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Conv1D(1024, 10, padding="same"))
     model.add(LeakyReLU())
-    # model.add(Conv1D(1024, 2, padding="same"))
-    # model.add(LeakyReLU())
-    model.add(LSTM(256, kernel_initializer= glorot_uniform(), input_shape=(1024,20), return_sequences=True))
-    model.add(Conv1D(1024, 2, padding="same"))
-    model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.1, input_shape=(1024,20), return_sequences=True))
+    model.add(LSTM(128, dropout=0.3, recurrent_dropout=0.2, return_sequences=True))
     model.add(BatchNormalization())
-    # model.add(LSTM(128, kernel_initializer= glorot_uniform(), return_sequences=True))
-    # model.add(BatchNormalization())
-    model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.1, kernel_initializer= glorot_uniform(), return_sequences=True))
-    model.add(LSTM(256, recurrent_dropout=0.1))
-    model.add(Dense(2048, activation="relu", kernel_regularizer=l1(0.0001)))
-    # model.add(Flatten())
+    model.add(LSTM(256, recurrent_dropout=0.2))
+    model.add(Dense(2048, activation="relu", kernel_regularizer=l1(0.0005)))
     model.add(BatchNormalization())
-    model.add(Dropout(0.1))
-    # model.add(Dense(2048, activation="relu"))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(2048, activation="relu"))
-    # model.add(Dropout(0.1))
-    model.add(Dense(512, kernel_regularizer=l1(0.0001), kernel_initializer= glorot_uniform(), activation="relu"))
-    # model.add(Dense(512, activation="relu"))
-    # model.add(Dropout(0.1))
-    # model.add(Dense(256, kernel_initializer= glorot_uniform(), activation="relu"))
-    # model.add(Dense(256, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Dense(512, kernel_regularizer=l1(0.0005), kernel_initializer= glorot_uniform(), activation="relu"))
     model.add(Dense(12, activation="softmax"))
     return model
 
 def train_model(dataset=None, model_path="./model/model.h5", labels=None, n_splits=5, learning_rate=0.0001, epochs=30, batch_size=64, verbose=True):
-    if verbose:
-        print("[INFO] Training!")
-    if dataset == None:
-        dataset, labels = load_data(path="./voices")
     kfold = StratifiedKFold(n_splits=n_splits, shuffle=True)
     
     model = give_model()
+
+    if verbose:
+        print("[INFO] Training!")
+        print(model.summary())
+
     model.compile(optimizer=RMSprop(learning_rate=learning_rate), loss="categorical_crossentropy", metrics=["acc"])
     
-    rlr = ReduceLROnPlateau(factor=0.5, verbose=1, patience=5, monitor="loss")
-    es = EarlyStopping(patience=8, verbose=1, restore_best_weights=True, monitor="loss")
+    rlr = ReduceLROnPlateau(factor=0.9, verbose=1, patience=8, monitor="val_loss")
     
     outs = np_utils.to_categorical(labels)
     hist = {}    
     for fold, (train_idx, val_idx) in enumerate(kfold.split(dataset, labels)):
         if verbose:
-            print(f"Shape of {fold} fold: (Train: {len(train_idx)}, Validation: {len(val_idx)})")
+            print(f"Shape of {fold+1} fold: (Train: {len(train_idx)}, Validation: {len(val_idx)})")
         Xtrain = generate_features(dataset[train_idx], datatype="train")
         ytrain = outs[train_idx]
         Xtest = generate_features(dataset[val_idx], datatype="valid")
@@ -78,7 +66,7 @@ def train_model(dataset=None, model_path="./model/model.h5", labels=None, n_spli
                     epochs=epochs,
                     verbose=verbose,
                     validation_data=(Xtest, ytest),
-                    callbacks=[rlr, es])
+                    callbacks=[rlr])
         model.save(model_path)
         hist[fold] = h.history
     if verbose:
